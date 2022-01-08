c747ae4891b5442f6be50ada48604158d7e1dc2e
icemanfromtopgun <tim.burns@me.com>
2022-01-08 11:47:56 -0500

    Created weather location struct

---
+++ WeatherGift/weatherlocation.swift
@@ -0,0 +1,15 @@
+//
+//  weatherlocation.swift
+//  WeatherGift
+//
+//  Created by Tim Burns on 1/8/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
