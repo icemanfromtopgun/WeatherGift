7a2960a11bdeddc49a04f93982059a7a7f7cd3ad
icemanfromtopgun <tim.burns@me.com>
2022-01-08 12:20:03 -0500

    Update

--- WeatherGift/ViewController.swift
+++ WeatherGift/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var WeatherLocations: [WeatherLocation] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+        
+        var weatherLocation = WeatherLocation(name: "Preston, UK", latitude: 0, longitude: 0)
+        WeatherLocations.append(weatherLocation)
+        weatherLocation = WeatherLocation(name: "Belfast, UK", latitude: 0, longitude: 0)
+        WeatherLocations.append(weatherLocation)
+        weatherLocation = WeatherLocation(name: "Melbourne, FL", latitude: 0, longitude: 0)
+        WeatherLocations.append(weatherLocation)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        }
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+     
+}
